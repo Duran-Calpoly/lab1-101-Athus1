8a860f6d8ea56b488cd2c8165db2bf6190a56392
Athus1 <93008511+Athus1@users.noreply.github.com>
2026-01-17 00:46:37 +0000

    finished lab

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,9 @@
-pass
+def calculate_average(num1:float,num2:float,num3:float)->float:
+    return(num1+num2+num3)/3
+
+def add_tax(bill_total:float)->float:
+    bill_total += bill_total*0.1
+    return bill_total
+
+def greet_user(name:str)->str:
+    return "Hello "+name
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,22 @@
-pass
+def check_multiple(number:int)->bool:
+    if (number%3==0) and (number%5==00):
+        return True
+    else:
+        return False
+    
+def check_password(input_string: str)->str:
+    secret_word = "Python123"
+    if input_string == secret_word:
+        return "access granted"
+    else:
+        return "access denied"
+    
+def calculate_federal_tax(salary:int)->float:
+    if(salary<=11000):
+        return salary*0.10
+    elif(salary<44725):
+        return salary*0.12
+    elif(salary<95375):    
+        return salary*0.22
+    else:
+        return salary*0.24
